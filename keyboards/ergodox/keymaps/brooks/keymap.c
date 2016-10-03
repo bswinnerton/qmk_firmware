f988ef5ce0cdeda2e47275de9ce9159618fd78bb
Brooks Swinnerton <bswinnerton@gmail.com>
2016-10-03 12:12:29 -0400

    Fix equal key

--- keyboards/ergodox/keymaps/brooks/keymap.c
+++ keyboards/ergodox/keymaps/brooks/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_SPC,   KC_BSPC,  KC_VOLD,
 
         // Right Hand
-        KC_FN1,         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_PEQL,
+        KC_FN1,         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
         KC_RBRC,        KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_MINS,
                         KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_SLSH,        KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_UP,    KC_RSFT,
