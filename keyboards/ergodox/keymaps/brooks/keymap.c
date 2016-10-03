f78a583c3a7583b1186612d7ed247dbab20c4587
Brooks Swinnerton <bswinnerton@gmail.com>
2016-10-02 22:41:23 -0400

    Fix layer toggle maps

--- keyboards/ergodox/keymaps/brooks/keymap.c
+++ keyboards/ergodox/keymaps/brooks/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
 
         // Left Hand
-        KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     TG(BASE),
+        KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_FN0,
         KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRACKET,
         CTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
         KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_BSLASH,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_SPC,   KC_BSPC,  KC_VOLD,
 
         // Right Hand
-        TG(SYMB),       KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_PEQL,
+        KC_FN1,         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_PEQL,
         KC_RBRC,        KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_MINS,
                         KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_SLSH,        KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_UP,    KC_RSFT,
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = KEYMAP(
        // Left Hand
-       M(0),            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    TG(BASE),
+       M(0),            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_FN0,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                           KC_TRNS,
                                                       KC_BTN1,  KC_BTN1,  KC_TRNS,
        // Right hand
-       TG(BASE),        KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
+       KC_FN1,          KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_F12,
                         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MS_U,  KC_TRNS,
@@ -101,7 +101,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+    [0] = ACTION_LAYER_SET(BASE, true),
+    [1] = ACTION_LAYER_SET(SYMB, true)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
