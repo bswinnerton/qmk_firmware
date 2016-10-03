73d050f82c8d35b9599198e37e2b61007e1987c4
Brooks Swinnerton <bswinnerton@gmail.com>
2016-10-03 18:19:57 -0400

    Remap alt and command

--- keyboards/ergodox/keymaps/brooks/keymap.c
+++ keyboards/ergodox/keymaps/brooks/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  \   |           |   /  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  Up  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      | LAlt | LAlt | LGui | LGui |                                       | RGui | RAlt | Rght | Down | Left |
+ *   |      |      | LAlt | LAlt | LGui |                                       | RGui | RAlt | Rght | Down | Left |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | ESC  | Mute |       | Pl/Ps|  ESC |
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRACKET,
         CTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
         KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_BSLASH,
-        KC_TRNS,        KC_LALT,  KC_LALT,  KC_LGUI,  KC_LGUI,
+        KC_TRNS,        KC_TRNS,  KC_LALT,  KC_LALT,  KC_LGUI,
 
                                                                 KC_ESC,   KC_MUTE,
                                                                           KC_MPRV,
