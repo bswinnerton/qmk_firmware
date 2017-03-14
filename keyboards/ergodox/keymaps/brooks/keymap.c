ab438178bc1564df94e72334c37f1a06946405fb
Brooks Swinnerton <bswinnerton@gmail.com>
2017-03-13 21:42:43 -0400

    Add extra layer for control to escape

--- keyboards/ergodox/keymaps/brooks/keymap.c
+++ keyboards/ergodox/keymaps/brooks/keymap.c
@@ -4,14 +4,62 @@
 #include "version.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
+#define ECTL 1 // default layer
+#define SYMB 2 // symbols
 #define EPRM M(1) // Macro 1: Reset EEPROM
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  | LAY2 |           | LAY2 |   6  |   7  |   8  |   9  |   0  |   =    |
+ * |   `    |   1  |   2  |   3  |   4  |   5  | LAY0 |           | LAY1 |   6  |   7  |   8  |   9  |   0  |   =    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   -    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |  Ctrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|  \   |           |   /  |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  Up  | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      | LAlt | LAlt | LGui |                                       | RGui | RAlt | Left | Down | Rght |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | ESC  | Mute |       | Pl/Ps|  ESC |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | Prev |       | Next |      |      |
+ *                                 |Space | Back |------|       |------| Enter| Space|
+ *                                 |      | space| Vol- |       | Vol+ |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[BASE] = KEYMAP(  // layer 0 : default
+
+        // Left Hand
+        KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_FN0,
+        KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRACKET,
+        KC_LCTL,        KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
+        KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_BSLASH,
+        KC_TRNS,        KC_TRNS,  KC_LALT,  KC_LALT,  KC_LGUI,
+
+                                                                KC_ESC,   KC_MUTE,
+                                                                          KC_MPRV,
+                                                      KC_SPC,   KC_BSPC,  KC_VOLD,
+
+        // Right Hand
+        KC_FN1,         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
+        KC_RBRC,        KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_MINS,
+                        KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+        KC_SLSH,        KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_UP,    KC_RSFT,
+                                  KC_RGUI,  KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT,
+
+        KC_MPLY,  KC_ESC,
+        KC_MNXT,
+        KC_VOLU,  KC_ENT, KC_SPC
+    ),
+/* Keymap 1: Basic layer with control as escape
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   `    |   1  |   2  |   3  |   4  |   5  | LAY0 |           | LAY2 |   6  |   7  |   8  |   9  |   0  |   =    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -31,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[BASE] = KEYMAP(  // layer 0 : default
+[ECTL] = KEYMAP(  // layer 0 : default
 
         // Left Hand
-        KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_FN1,
+        KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_FN0,
         KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_LBRACKET,
         CTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,
         KC_LSFT,        KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_BSLASH,
@@ -45,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_SPC,   KC_BSPC,  KC_VOLD,
 
         // Right Hand
-        KC_FN1,         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
+        KC_FN2,         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQL,
         KC_RBRC,        KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_MINS,
                         KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
         KC_SLSH,        KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_UP,    KC_RSFT,
@@ -58,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |Version |  F1  |  F2  |  F3  |  F4  |  F5  | LAY1 |           | LAY1 |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |Version |  F1  |  F2  |  F3  |  F4  |  F5  | LAY1 |           | LAY2 |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -79,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
 [SYMB] = KEYMAP(
        // Left Hand
-       M(0),            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_FN0,
+       M(0),            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_FN1,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -89,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                           KC_TRNS,
                                                       KC_BTN1,  KC_BTN1,  KC_TRNS,
        // Right hand
-       KC_FN0,          KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
+       KC_FN2,          KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_F12,
                         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MS_U,  KC_TRNS,
@@ -102,7 +150,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_SET(BASE, true),
-    [1] = ACTION_LAYER_SET(SYMB, true)
+    [1] = ACTION_LAYER_SET(ECTL, true),
+    [2] = ACTION_LAYER_SET(SYMB, true)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
