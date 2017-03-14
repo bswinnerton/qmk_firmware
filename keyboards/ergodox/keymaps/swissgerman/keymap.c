86fe447398959e384d50e5d94a061f0fcb216124
Brooks Swinnerton <bswinnerton@gmail.com>
2017-03-13 21:26:58 -0400

    Don't make changes to swissgerman's keymaps

--- keyboards/ergodox/keymaps/swissgerman/keymap.c
+++ keyboards/ergodox/keymaps/swissgerman/keymap.c
@@ -8,6 +8,7 @@
 #include "visualizer/lcd_backlight.h"
 #endif
 
+
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
@@ -273,7 +274,7 @@ void send_key(uint16_t keycode) {
 
 void go_back_based_on_tag(char* tag) {
   const int BRACKETS_AND_SLASH_LENGTH = 3;
-
+  
   for (int i=0; i < strlen(tag) + BRACKETS_AND_SLASH_LENGTH; i++) {
     send_key(KC_LEFT);
   }
